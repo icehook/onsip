2e80b6329db40b7e1e7052b685884044b2123303
brendon9 <brendon.harris9@gmail.com>
2014-12-05 16:58:08 -0500

    user add spec updated

--- spec/lib/onsip/models/external_address_spec.rb
+++ spec/lib/onsip/models/external_address_spec.rb
@@ -2,15 +2,17 @@ require 'spec_helper'
 
 describe OnSIP::ExternalAddress do
   before :each do
-
-    body = {"Response"=>{"Context"=>{"Action"=>{"IsCompleted"=>"true"}, "Request"=>{"IsValid"=>"true", "DateTime"=>"2014-12-04T17:39:53+00:00", "Duration"=>"30", "Parameters"=>{"Parameter"=>[{"Name"=>"Action", "Value"=>"SessionCreate"}, {"Name"=>"Output", "Value"=>"json"}, {"Name"=>"Password", "Value"=>"******"}, {"Name"=>"Username", "Value"=>"icehook"}]}}, "Session"=>{"IsEstablished"=>"true", "SessionId"=>"j2vtu1bk7rsdpklofplu07ocq1", "UserId"=>"212762", "Roles"=>{"Role"=>{"Name"=>"Account Admin"}}}}, "Result"=>{"SessionCreate"=>{}}}}.to_json
-    stub_request(:get, Regexp.new('http://api.onsip.com/api?.*ExternalAddressAdd.*'))
-    .to_return({
-      :body => body,
-      :status => 200,
-      :headers => { 'Content-Type' => 'application/json; charset=utf-8' }
-    })
     OnSIP.connect('http://api.onsip.com')
+    OnSIP.session = OnSIP::Session.new("IsEstablished" => "true")
   end
 
+  describe "browse" do
+    body =  {"Response"=>{"Context"=>{"Action"=>{"IsCompleted"=>"true"}, "Request"=>{"IsValid"=>"true", "DateTime"=>"2014-12-05T20:13:59+00:00", "Duration"=>"17", "Parameters"=>{"Parameter"=>[{"Name"=>"Action", "Value"=>"ExternalAddressBrowse"}, {"Name"=>"Output", "Value"=>"json"}, {"Name"=>"SessionId", "Value"=>"lu2qftbuv883gu5iu5qofrva90"}, {"Name"=>"UserId", "Value"=>"266848"}, {"Name"=>"Limit", "Value"=>"20"}, {"Name"=>"Offset", "Value"=>"0"}, {"Name"=>"CalcFound", "Value"=>"true"}]}}, "Session"=>{"IsEstablished"=>"true", "SessionId"=>"lu2qftbuv883gu5iu5qofrva90", "UserId"=>"212762", "Roles"=>{"Role"=>{"Name"=>"Account Admin"}}}}, "Result"=>{"ExternalAddressBrowse"=>{"ExternalAddresses"=>{"@attributes"=>{"Limit"=>"20", "Offset"=>"0"}}}}}}.to_json
+    before { custom_stub(body, 'api', 'ExternalAddressBrowse') }
+
+    it "with valid id" do
+      ea = OnSIP::ExternalAddress.browse({'UserId' => '266848'})
+      expect(ea).to be_empty
+    end
+  end
 end
--- spec/lib/onsip/models/user_spec.rb
+++ spec/lib/onsip/models/user_spec.rb
@@ -3,19 +3,50 @@ require 'spec_helper'
 describe OnSIP::User do
 
   before :each do
-    OnSIP.connect('http://api.onsip.com')
     OnSIP.session = OnSIP::Session.new("IsEstablished" => "true")
   end
 
   describe "read" do
-    body = {"Response"=>{"Context"=>{"Action"=>{"IsCompleted"=>"true"}, "Request"=>{"IsValid"=>"true", "DateTime"=>"2014-12-04T20:17:17+00:00", "Duration"=>"46", "Parameters"=>{"Parameter"=>[{"Name"=>"Action", "Value"=>"UserRead"}, {"Name"=>"Output", "Value"=>"json"}, {"Name"=>"SessionId", "Value"=>"bvb4thhuk1d7tb2bddf09teqr4"}, {"Name"=>"UserId", "Value"=>"260161"}]}}, "Session"=>{"IsEstablished"=>"true", "SessionId"=>"bvb4thhuk1d7tb2bddf09teqr4", "UserId"=>"212762", "Roles"=>{"Role"=>{"Name"=>"Account Admin"}}}}, "Result"=>{"UserRead"=>{"User"=>{"UserId"=>"260161", "OrganizationId"=>"30096", "AccountId"=>"27370", "Flags"=>"32", "Status"=>"disabled", "Domain"=>"sip.firertc.com", "Username"=>"test3_firertc", "Password"=>"**********", "AuthUsername"=>"test3_firertc", "Modified"=>"2014-11-24T19:24:03+00:00", "Created"=>"2014-11-03T19:30:08+00:00", "Ack911"=>"1970-01-01T00:00:00+00:00", "SurveyCompleted"=>"1970-01-01T00:00:00+00:00", "AckTrunkingTerms"=>"1970-01-01T00:00:00+00:00", "AckTrunkingE911"=>"1970-01-01T00:00:00+00:00", "AckHostedTerms"=>"1970-01-01T00:00:00+00:00", "AckHostedE911"=>"1970-01-01T00:00:00+00:00", "ExtendedDialing"=>"false", "PSTNForwarding"=>"false", "E911Provisioning"=>"false", "PSTNTrunking"=>"false", "FreePSTNTrunking"=>"false", "BusyLampField"=>"true", "Contact"=>{"Name"=>"test", "Organization"=>"IceHook Systems", "Address"=>"123 Main St", "City"=>"New York", "State"=>"NY", "Zipcode"=>"10000", "Country"=>"United States of America", "CountryId"=>"207", "Phone"=>"1-212-555-1212", "Email"=>"test@icehook.com", "Modified"=>"2014-11-03T19:30:08+00:00"}, "Roles"=>{"Role"=>{"Name"=>"User"}}}}}}}.to_json
-    before { custom_stub(body, 'api', 'UserRead') }
+    read_body = {"Response"=>{"Context"=>{"Action"=>{"IsCompleted"=>"true"}, "Request"=>{"IsValid"=>"true", "DateTime"=>"2014-12-04T20:17:17+00:00", "Duration"=>"46", "Parameters"=>{"Parameter"=>[{"Name"=>"Action", "Value"=>"UserRead"}, {"Name"=>"Output", "Value"=>"json"}, {"Name"=>"SessionId", "Value"=>"bvb4thhuk1d7tb2bddf09teqr4"}, {"Name"=>"UserId", "Value"=>"260161"}]}}, "Session"=>{"IsEstablished"=>"true", "SessionId"=>"bvb4thhuk1d7tb2bddf09teqr4", "UserId"=>"212762", "Roles"=>{"Role"=>{"Name"=>"Account Admin"}}}}, "Result"=>{"UserRead"=>{"User"=>{"UserId"=>"260161", "OrganizationId"=>"30096", "AccountId"=>"27370", "Flags"=>"32", "Status"=>"disabled", "Domain"=>"sip.firertc.com", "Username"=>"test3_firertc", "Password"=>"**********", "AuthUsername"=>"test3_firertc", "Modified"=>"2014-11-24T19:24:03+00:00", "Created"=>"2014-11-03T19:30:08+00:00", "Ack911"=>"1970-01-01T00:00:00+00:00", "SurveyCompleted"=>"1970-01-01T00:00:00+00:00", "AckTrunkingTerms"=>"1970-01-01T00:00:00+00:00", "AckTrunkingE911"=>"1970-01-01T00:00:00+00:00", "AckHostedTerms"=>"1970-01-01T00:00:00+00:00", "AckHostedE911"=>"1970-01-01T00:00:00+00:00", "ExtendedDialing"=>"false", "PSTNForwarding"=>"false", "E911Provisioning"=>"false", "PSTNTrunking"=>"false", "FreePSTNTrunking"=>"false", "BusyLampField"=>"true", "Contact"=>{"Name"=>"test", "Organization"=>"IceHook Systems", "Address"=>"123 Main St", "City"=>"New York", "State"=>"NY", "Zipcode"=>"10000", "Country"=>"United States of America", "CountryId"=>"207", "Phone"=>"1-212-555-1212", "Email"=>"test@icehook.com", "Modified"=>"2014-11-03T19:30:08+00:00"}, "Roles"=>{"Role"=>{"Name"=>"User"}}}}}}}.to_json
+    before { custom_stub(read_body, 'read', 'UserRead') }
 
     it "with valid id" do
+      OnSIP.connect('http://read.onsip.com')
       user = OnSIP::User.read('260161')
       expect(user.class).to eq OnSIP::User
     end
 
+  end
+
+  describe "add" do
+    add_body = {"Response"=>{"Context"=>{"Action"=>{ "IsCompleted"=>"true" },"Request"=>{"IsValid"=>"true","DateTime"=>"2014-04-21T16:19:55+00:00","Duration"=>"165","Parameters"=>{"Parameter"=>[{"Name"=>"Action","Value"=>"UserAdd"},{"Name"=>"SessionId","Value"=>"thqqva0i6jp3ea1ved17shcdg7"},{"Name"=>"OrganizationId","Value"=>"25018"},{"Name"=>"Username","Value"=>"docs"},{"Name"=>"Domain","Value"=>"example.onsip.com"},{"Name"=>"Name","Value"=>"Docs"},{"Name"=>"Email","Value"=>"docs@example.onsip.com"},{"Name"=>"AuthUsername","Value"=>"example"},{"Name"=>"Password","Value"=>"******"},{"Name"=>"PasswordConfirm","Value"=>"******"},{"Name"=>"Role","Value"=>"User"}]}},"Session"=>{"IsEstablished"=>"true","SessionId"=>"thqqva0i6jp3ea1ved17shcdg7","UserId"=>"152255","Roles"=>{"Role"=>{ "Name"=>"Account Admin" }}}},"Result"=>{"UserAdd"=>{"User"=>{"UserId"=>"218339","OrganizationId"=>"25018","AccountId"=>"22543","Flags"=>"0","Status"=>"enabled","Domain"=>"example.onsip.com","Username"=>"example","Password"=>"QPD2MLQhdaHB2hBW","AuthUsername"=>"example","Modified"=>"2014-04-21T16:19:55+00:00","Created"=>"2014-04-21T16:19:55+00:00","Ack911"=>"1970-01-01T00:00:00+00:00","SurveyCompleted"=>"1970-01-01T00:00:00+00:00","AckTrunkingTerms"=>"1970-01-01T00:00:00+00:00","AckTrunkingE911"=>"1970-01-01T00:00:00+00:00","AckHostedTerms"=>"1970-01-01T00:00:00+00:00","AckHostedE911"=>"1970-01-01T00:00:00+00:00","ExtendedDialing"=>"false","PSTNForwarding"=>"false","E911Provisioning"=>"false","PSTNTrunking"=>"false","FreePSTNTrunking"=>"false","BusyLampField"=>"false","Contact"=>{"Name"=>"Docs","Organization"=>"example","Address"=>"55 broad street","City"=>"Brooklyn","State"=>"NY","Zipcode"=>"11201","Country"=>"United States of America","CountryId"=>"207","Phone"=>"1-555-555-5555","Email"=>"docs@example.onsip.com","Modified"=>"2014-04-21T16:19:55+00:00"},"Roles"=>{"Role"=>{ "Name"=>"User" }}}}}}}.to_json
+    before {
+      stub_request(:get, Regexp.new("http://add.onsip.com/api?.*UserAdd.*"))
+      .to_return({
+        :body => add_body,
+        :status => 200,
+        :headers => { 'Content-Type' => 'application/json; charset=utf-8' }
+        })
+    }
+
+
+    it "with valid orgnization and attrs" do
+      OnSIP.connect('http://add.onsip.com')
+      org = OnSIP::Organization.new
+      attrs = {
+        'SessionId' => 'thqqva0i6jp3ea1ved17shcdg7',
+        'OrganizationId' => '2501',
+        'Username' => 'docs',
+        'Domain' => 'example.onsip.com',
+        'Name' => 'Docs',
+        'Email' => 'docs@example.onsip.com',
+        'AuthUsername' => 'example',
+        'Password' => 'mysuperpassword',
+        'PasswordConfirm' => 'mysuperpassword'
+      }
+      user = OnSIP::User.add(org, attrs)
+      expect(user.class).to eq OnSIP::User
+    end
   end
 
 end
