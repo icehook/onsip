a84e4c8ddb8dd10ab335dbaa65168a2fff8c0603
Keith Larrimore <keithlarrimore@gmail.com>
2015-06-01 17:15:30 -0400

    test for exception on taken username

--- spec/lib/onsip/models/user_spec.rb
+++ spec/lib/onsip/models/user_spec.rb
@@ -19,8 +19,16 @@ describe OnSIP::User do
   end
 
   describe "add" do
+    fail_body = {"Response"=> {"Context"=> {"Action"=>{"IsCompleted"=>"false"}, "Request"=> {"IsValid"=>"false", "DateTime"=>"2015-06-01T18:45:16+00:00", "Duration"=>"38", "Parameters"=> {"Parameter"=> [{"Name"=>"Action", "Value"=>"UserAdd"}, {"Name"=>"AuthUsername", "Value"=>"foobar888_firertc"}, {"Name"=>"Domain", "Value"=>"sip.devel.firertc.com"}, {"Name"=>"Email", "Value"=>"onsip+foobar888@icehook.com"}, {"Name"=>"Name", "Value"=>{}}, {"Name"=>"OrganizationId", "Value"=>"42975"}, {"Name"=>"Output", "Value"=>"json"}, {"Name"=>"Password", "Value"=>"******"}, {"Name"=>"PasswordConfirm", "Value"=>"******"}, {"Name"=>"SessionId", "Value"=>"ak7gllnq56e0soiebn23h9rlu3"}, {"Name"=>"Username", "Value"=>"foobar888"}]}, "Errors"=>{"Error"=>{"Parameter"=>"Username", "Code"=>"Address.Unavailable", "Message"=>"This address is already in use, is reserved, or is otherwise unavailable for use."}}}, "Session"=>{"IsEstablished"=>"true", "SessionId"=>"ak7gllnq56e0soiebn23h9rlu3", "UserId"=>"282881", "Roles"=>{"Role"=>{"Name"=>"Account Admin"}}}}}}.to_json
     add_body = {"Response"=>{"Context"=>{"Action"=>{ "IsCompleted"=>"true" },"Request"=>{"IsValid"=>"true","DateTime"=>"2014-04-21T16:19:55+00:00","Duration"=>"165","Parameters"=>{"Parameter"=>[{"Name"=>"Action","Value"=>"UserAdd"},{"Name"=>"SessionId","Value"=>"thqqva0i6jp3ea1ved17shcdg7"},{"Name"=>"OrganizationId","Value"=>"25018"},{"Name"=>"Username","Value"=>"docs"},{"Name"=>"Domain","Value"=>"example.onsip.com"},{"Name"=>"Name","Value"=>"Docs"},{"Name"=>"Email","Value"=>"docs@example.onsip.com"},{"Name"=>"AuthUsername","Value"=>"example"},{"Name"=>"Password","Value"=>"******"},{"Name"=>"PasswordConfirm","Value"=>"******"},{"Name"=>"Role","Value"=>"User"}]}},"Session"=>{"IsEstablished"=>"true","SessionId"=>"thqqva0i6jp3ea1ved17shcdg7","UserId"=>"152255","Roles"=>{"Role"=>{ "Name"=>"Account Admin" }}}},"Result"=>{"UserAdd"=>{"User"=>{"UserId"=>"218339","OrganizationId"=>"25018","AccountId"=>"22543","Flags"=>"0","Status"=>"enabled","Domain"=>"example.onsip.com","Username"=>"example","Password"=>"QPD2MLQhdaHB2hBW","AuthUsername"=>"example","Modified"=>"2014-04-21T16:19:55+00:00","Created"=>"2014-04-21T16:19:55+00:00","Ack911"=>"1970-01-01T00:00:00+00:00","SurveyCompleted"=>"1970-01-01T00:00:00+00:00","AckTrunkingTerms"=>"1970-01-01T00:00:00+00:00","AckTrunkingE911"=>"1970-01-01T00:00:00+00:00","AckHostedTerms"=>"1970-01-01T00:00:00+00:00","AckHostedE911"=>"1970-01-01T00:00:00+00:00","ExtendedDialing"=>"false","PSTNForwarding"=>"false","E911Provisioning"=>"false","PSTNTrunking"=>"false","FreePSTNTrunking"=>"false","BusyLampField"=>"false","Contact"=>{"Name"=>"Docs","Organization"=>"example","Address"=>"55 broad street","City"=>"Brooklyn","State"=>"NY","Zipcode"=>"11201","Country"=>"United States of America","CountryId"=>"207","Phone"=>"1-555-555-5555","Email"=>"docs@example.onsip.com","Modified"=>"2014-04-21T16:19:55+00:00"},"Roles"=>{"Role"=>{ "Name"=>"User" }}}}}}}.to_json
     before {
+      stub_request(:get, Regexp.new("http://addfail.onsip.com/api?.*UserAdd.*"))
+      .to_return({
+        :body => fail_body,
+        :status => 200,
+        :headers => { 'Content-Type' => 'application/json; charset=utf-8' }
+        })
+
       stub_request(:get, Regexp.new("http://add.onsip.com/api?.*UserAdd.*"))
       .to_return({
         :body => add_body,
@@ -29,8 +37,7 @@ describe OnSIP::User do
         })
     }
 
-
-    it "with valid orgnization and attrs" do
+    it "should add with valid orgnization and attrs" do
       OnSIP.connect('http://add.onsip.com')
       org = OnSIP::Organization.new
       attrs = {
@@ -46,6 +53,24 @@ describe OnSIP::User do
       }
       user = OnSIP::User.add(org, attrs)
       expect(user.class).to eq OnSIP::User
+    end
+
+    it "should not add without valid orgnization and attrs" do
+      OnSIP.connect('http://addfail.onsip.com')
+      org = OnSIP::Organization.new
+      attrs = {
+        'SessionId' => 'thqqva0i6jp3ea1ved17shcdg7',
+        'OrganizationId' => '2501',
+        'Username' => 'docs',
+        'Domain' => 'example.onsip.com',
+        'Name' => 'Docs',
+        'Email' => 'docs@example.onsip.com',
+        'AuthUsername' => 'example',
+        'Password' => 'mysuperpassword',
+        'PasswordConfirm' => 'mysuperpassword'
+      }
+      expect { OnSIP::User.add(org, attrs) }.to raise_error(OnSIP::Exceptions::OnSIPRequestError) 
+
     end
   end
 
--- spec/lib/onsip/session_spec.rb
+++ spec/lib/onsip/session_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 describe OnSIP::Session do
   before :each do
 
+    # {"Response"=>{"Context"=>{"Action"=>{"IsCompleted"=>"false"}, "Request"=>{"IsValid"=>"true", "DateTime"=>"2014-12-04T20:07:08+00:00", "Duration"=>"8", "Parameters"=>{"Parameter"=>[{"Name"=>"Action", "Value"=>"SessionCreate"}, {"Name"=>"Output", "Value"=>"json"}, {"Name"=>"Password", "Value"=>"7onsip6"}, {"Name"=>"Username", "Value"=>"klarrimore@icehook.com"}]}, "Errors"=>{"Error"=>{"Parameter"=>"Action", "Code"=>"Accessor.LoginLocked", "Message"=>"You have failed to log in too many times. Please wait 10 minutes then try again."}}}, "Session"=>{"IsEstablished"=>"false"}}}}
+    
     body = {"Response"=>{"Context"=>{"Action"=>{"IsCompleted"=>"true"}, "Request"=>{"IsValid"=>"true", "DateTime"=>"2014-12-04T17:39:53+00:00", "Duration"=>"30", "Parameters"=>{"Parameter"=>[{"Name"=>"Action", "Value"=>"SessionCreate"}, {"Name"=>"Output", "Value"=>"json"}, {"Name"=>"Password", "Value"=>"******"}, {"Name"=>"Username", "Value"=>"icehook"}]}}, "Session"=>{"IsEstablished"=>"true", "SessionId"=>"j2vtu1bk7rsdpklofplu07ocq1", "UserId"=>"212762", "Roles"=>{"Role"=>{"Name"=>"Account Admin"}}}}, "Result"=>{"SessionCreate"=>{}}}}.to_json
     stub_request(:get, Regexp.new('http://api.onsip.com/api?.*SessionCreate.*'))
     .to_return({
@@ -27,5 +29,3 @@ describe OnSIP::Session do
   end
 
 end
-
-# {"Response"=>{"Context"=>{"Action"=>{"IsCompleted"=>"false"}, "Request"=>{"IsValid"=>"true", "DateTime"=>"2014-12-04T20:07:08+00:00", "Duration"=>"8", "Parameters"=>{"Parameter"=>[{"Name"=>"Action", "Value"=>"SessionCreate"}, {"Name"=>"Output", "Value"=>"json"}, {"Name"=>"Password", "Value"=>"7onsip6"}, {"Name"=>"Username", "Value"=>"klarrimore@icehook.com"}]}, "Errors"=>{"Error"=>{"Parameter"=>"Action", "Code"=>"Accessor.LoginLocked", "Message"=>"You have failed to log in too many times. Please wait 10 minutes then try again."}}}, "Session"=>{"IsEstablished"=>"false"}}}}
